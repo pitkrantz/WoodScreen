fcf9319836af5d499ff838401e62be30a71b2377
pitkrantz <pitkrantz@gmail.com>
2022-07-03 21:54:29 +0200

    Initial Commit

---
+++ WoodScreen/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WoodScreen
+//
+//  Created by Pit Krantz on 03/07/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WoodScreen/WoodScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  WoodScreenApp.swift
+//  WoodScreen
+//
+//  Created by Pit Krantz on 03/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct WoodScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
